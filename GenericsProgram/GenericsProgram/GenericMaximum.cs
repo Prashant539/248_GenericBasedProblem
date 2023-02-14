9d1869319e1cb2a8df1207be726fe569dfa5d430
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-02-14 16:59:16 +0530

    [Prashant]Add: Refactor Generic class and find Max value

--- GenericsProgram/GenericsProgram/GenericMaximum.cs
+++ GenericsProgram/GenericsProgram/GenericMaximum.cs
@@ -8,6 +8,14 @@ namespace GenericsProgram
 {
     public class GenericMaximum<T> where T : IComparable
     {
+        public T firstValue, secondValue, thirdValue;
+        //parameteric constructor
+        public GenericMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
         public static T MaxValueCheck(T firstValue, T secondValue, T thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
@@ -24,5 +32,6 @@ namespace GenericsProgram
             }
             throw new Exception("firstValue ,SecondValue and thirdValue are same");
         }
+       
     }
 }
